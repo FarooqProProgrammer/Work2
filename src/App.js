8b644668c2abc5a296c516e8fbb54b7b7dfcc9aa
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-04-07 16:07:53 +0500

    UPDATED

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import Routing from './Routing'
 import "./App.css"
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './Redux/Store'
 
 
 
 export default function App() {
-  return <Routing />
+  return (
+
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routing />
+      </PersistGate>
+    </Provider>
+
+
+  )
 }
